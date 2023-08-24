2bb6079782b401a55669fd1f511bfd343c51019d
Cristian Juarez <84170516+cristianjuarezz@users.noreply.github.com>
2023-08-24 19:18:12 -0300

    Update extract_locale.py
    Commented use of non existant file

--- extract_locale.py
+++ extract_locale.py
@@ -22,8 +22,8 @@ def process(fn: str):
 print("processing infer-web.py")
 process("infer-web.py")
 
-print("processing gui_v0.py")
-process("gui_v0.py")
+#print("processing gui_v0.py")
+#process("gui_v0.py")
 
 print("processing gui_v1.py")
 process("gui_v1.py")
